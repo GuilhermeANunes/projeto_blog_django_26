8593d24bc3684414eb87e2d464dac7167d1c51f4
Guilherme Nunes <gui_augustonunes@outlook.com>
2026-05-16 02:41:14 -0300

    Making partials, configuring admin page, favicon and css

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from site_setup.models import MenuLinks, SiteSetup
 
 # Register your models here.
+# @admin.register(MenuLinks)
+# class MenuLinksAdmin(admin.ModelAdmin):
+#     list_display = 'id', 'text', 'url_or_path'
+#     list_display_links = 'id', 'text', 'url_or_path'
+#     search_fields = 'id', 'text', 'url_or_path'
+
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLinks
+    extra = 1
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description'
+    inlines = MenuLinkInline,
+
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists()
---
+++ djangoapp/utils/image.py
@@ -0,0 +1,23 @@
+from PIL import Image
+from pathlib import Path
+from django.conf import settings
+
+def resize_image(image_django, new_width=32, optimize=True, quality=60):
+    image_path = Path(settings.MEDIA_ROOT / image_django.name).resolve()
+    image_pillow = Image.open(image_path)
+    original_width, original_heigth = image_pillow.size
+
+    if original_width <= new_width:
+        image_pillow.close()
+        return image_pillow
+    
+    new_heigth = round(new_width * original_heigth / original_width)
+
+    new_image = image_pillow.resize((new_width, new_heigth), Image.LANCZOS)
+    new_image.save(
+        image_path,
+        optimize=optimize,
+        quality=quality
+    )
+
+    return new_image
